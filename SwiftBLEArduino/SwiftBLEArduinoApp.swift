184d2f7cc287fa5a2e1c585e679fe3342732fd66
svmadap <81061506+suchir-madap@users.noreply.github.com>
2024-08-23 11:28:09 -0400

    Initial Commit

---
+++ SwiftBLEArduino/SwiftBLEArduinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBLEArduinoApp.swift
+//  SwiftBLEArduino
+//
+//  Created by Madap on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBLEArduinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
